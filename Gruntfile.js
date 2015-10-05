6ab1731edbe3dbdc94a3749d61bf2947973ab350
erinlehmkuhl <erinlehmkuhl@gmail.com>
2015-10-05 16:19:07 -0700

    refactor: compress, inline, optimize

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+          target: {
+          files: {
+              'js/app.min.js': ['js/src/app.js'],
+          }
+      }
+    },
+
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'images/src',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'images/'
+            }]
+        }
+    },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+          roundingPrecision: -1
+        },
+        target: {
+          files: {
+              'css/style.min.css': ['css/style.css'],
+              'css/inline.min.css': ['css/inline.css']
+          }
+      }
+    },
+
+    htmlmin: {                                     // Task 
+        dist: {                                      // Target 
+          options: {                                 // Target options 
+            removeComments: true,
+              collapseWhitespace: true
+            },
+          files: {                                   // Dictionary of files 
+              'index.html': 'src/index.html',     // 'destination': 'source' 
+            }
+        }
+      },
+
+    // gzip assets 1-to-1 for production
+    // compress: {
+    //   main: {
+    //     options: {
+    //       mode: 'gzip'
+    //     },
+    //     expand: true,
+    //     src: ['**/*'],
+    //     dest: 'public/'
+    //   }
+    // }
+
+    });
+
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    //grunt.loadNpmTasks('grunt-contrib-compress');
+
+
+    grunt.registerTask('default', ['uglify', 'imagemin', 'cssmin', 'htmlmin']);
+
+};
